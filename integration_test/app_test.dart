932965e2304e7a774e8fc9a7949c1c76a5db5e5d
PauloVOFonseca <paulovitoradsifba@gmail.com>
2023-02-10 09:12:10 -0300

    feat: start integration test

---
+++ integration_test/app_test.dart
@@ -0,0 +1,25 @@
+import 'package:exchange_house_app/main.dart' as app;
+import 'package:exchange_house_app/src/presentation/widgets/custom_button.dart';
+import 'package:exchange_house_app/src/presentation/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Home Page 2E2 test', () {
+    testWidgets('Test conversion request with textEditingController',
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      await tester.enterText(find.byType(CustomTextField), '2');
+      await tester.pumpAndSettle();
+
+      expect(find.text('2'), findsOneWidget);
+
+      var button = find.byType(CustomButton);
+    });
+  });
+}
