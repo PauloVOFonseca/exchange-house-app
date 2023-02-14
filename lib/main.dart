3086c9ca5201c988b484e4a6b4ac3d9a9027fea4
PauloVOFonseca <paulovitoradsifba@gmail.com>
2023-02-14 17:09:09 -0300

    BREAKING CHANGE: deleting hive and install sqlite

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:exchange_house_app/src/application/services/hive_config.dart';
 import 'package:exchange_house_app/src/application/services/locator.dart';
 import 'package:exchange_house_app/src/presentation/app/app_widget.dart';
 import 'package:exchange_house_app/src/presentation/pages/home/home_controller.dart';
@@ -8,9 +7,7 @@ import 'package:provider/provider.dart';
 void main() {
   setupGetIt();
   WidgetsFlutterBinding.ensureInitialized();
-  Future.delayed(Duration.zero, () async {
-    await HiveConfig.initHive();
-  });
+
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => HomeController()),
   ], child: const AppWidget()));
